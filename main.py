48a32d18a7fd7725db875cf25c37bd158cc7b949
Seven-Down-svg <tynerrowell2@gmail.com>
2024-11-25 11:37:46 -0700

    Output to show the word count of book

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+FILENAME = "books/frankenstein.txt"
 
 #create a main function to read the book
 def main():
     #read the book
-    with open("books/frankenstein.txt") as f:
+    with open(FILENAME) as f:
         file_contents = f.read()
     #print the book
-    print(file_contents)
+    #print(file_contents)
+    #print the word count of the book
+    print(len(file_contents.split()))
 
 #call the main function
 if __name__ == "__main__":
